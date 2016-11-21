fecf21bfe8ed29108ba8fba14dc23d354c9aff3a
Akash S Prakash <meakashsprakashhere@gmail.com>
2016-11-21 18:04:20 +0530

    Added auto triggering to email validator

--- js/jquery.emailvalidator.js
+++ js/jquery.emailvalidator.js
@@ -10,19 +10,22 @@
         var email = this.val();
         if(checkIfValidEmail(email)){
 
-            $(""+msg+"").html("<p id=\"messge\">Valid Email</p>");
+            $(""+msg+"").html("<p id=\"valid\">Valid Email</p>");
             $(""+msg+"").css( "color", "green");
         }
         else{
-            $(""+msg+"").html("<p>Invalid Email</p>");
+            $(""+msg+"").html("<p id=\"invalid\">Invalid Email&nbsp</p>");
+            $("#invalid").append("<span style=\"font-size: 15px\" class=\"glyphicon glyphicon-remove-circle\"></span>");
             $(""+msg+"").css( "color", "red");
         }
     };
 
     function checkIfValidEmail(email){
-        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        var result = regex.test(email);
-        return result;
+        // if (count < maxlength) {
+            var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var result = regex.test(email);
+            return result;
+        // }
     }
 
 }( jQuery ));
